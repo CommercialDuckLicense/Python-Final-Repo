63a5a561a0f4637ad90d0aa0fdb29cab53bd9d21
CommercialDuckLicense <voelkeltmj@gmail.com>
2025-11-26 23:37:20 -0700

    while loops for the questions

--- Quiz.py
+++ Quiz.py
@@ -46,32 +46,27 @@ correctAnswerLCM=compute_lcm(LCM_num1, LCM_num2)
 #equaling the correct answer with the correct GCD value
 correctAnswerGCD=compute_GCD(GCFnum1, GCFnum2)
 
-#user input for the lcm
-print('First Question:')
-answerLCM= int(input(f'Find the LCM of {LCM_num1} and {LCM_num2} ->'))
-
-if answerLCM==correctAnswerLCM:
-    print ('Correct ')
-else:
-    print("Incorrect the L.C.M. is", compute_lcm(LCM_num1, LCM_num2))
 
 
-#user input for the GCD
-print('Second Question:')
-answerGCD= int(input(f'Find the GCD of {GCFnum1} and {GCFnum2} ->'))
+while True: # loop for when the question is wrong it loops until the answer is correct
+    #user input for the lcm
+    print('First Question:')
+    answerLCM= int(input(f'Find the LCM of {LCM_num1} and {LCM_num2} ->'))
 
-if answerGCD==correctAnswerGCD:
-    print ('Correct ')
-else:
-    print("Incorrect the G.C.D is", compute_GCD(GCFnum1, GCFnum2))
-
-QUESTIONS=[
-    (f"Find the LCM of {LCM_num1} and {LCM_num2} ->",correctAnswerLCM)
-    (f'Find the GCD of {GCFnum1} and {GCFnum2} ->', correctAnswerGCD)
-]
-for answerLCM,correctAnswerLCM in QUESTIONS:
-    answer=input(f"{answerLCM}?")
-    if answer ==correctAnswerLCM:
+    if answerLCM==correctAnswerLCM:
         print ('Correct ')
+        break
     else:
         print("Incorrect the L.C.M. is", compute_lcm(LCM_num1, LCM_num2))
+
+        
+while True:
+    #user input for the GCD
+    print('Second Question:')
+    answerGCD= int(input(f'Find the GCD of {GCFnum1} and {GCFnum2} ->'))
+
+    if answerGCD==correctAnswerGCD:
+        print ('Correct ')
+        break
+    else:
+        print("Incorrect the G.C.D is", compute_GCD(GCFnum1, GCFnum2))
