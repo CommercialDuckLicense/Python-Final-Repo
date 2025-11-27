aad974d6a40ba76f650b0a03317c9cc362ec946e
CommercialDuckLicense <voelkeltmj@gmail.com>
2025-11-26 22:45:36 -0700

    LCM and GCD questions

--- Quiz.py
+++ Quiz.py
@@ -1,11 +1,12 @@
 import random
 
-#generate lcm questions 
+player=input(("Type your Name "))
+print(f'Hello {player} this is a quiz game to help with basic math questions.')
+print('Ready for the first question? ')
 
 
-# Python Program to find the L.C.M. of two input number
+# Function to find LCM
 def compute_lcm(x, y):
-
    # choose the greater number
    if x > y:
        greater = x
@@ -17,10 +18,60 @@ def compute_lcm(x, y):
            lcm = greater
            break
        greater += 1
-
    return lcm 
-num1=random.randint(1,20) 
-num2=random.randint(1,20)
+
+#function to find GCD
+def compute_GCD(x, y):
+# choose the smaller number
+    if x > y:
+        smaller = y
+    else:
+        smaller = x
+    for i in range(1, smaller+1):
+        if((x % i == 0) and (y % i == 0)):
+            GCD = i 
+    return GCD
+
+GCFnum1 =random.randint(1,10)
+GCFnum2 =random.randint(1,10)
+
+#generate random number for LCM
+LCM_num1=random.randint(1,10) 
+LCM_num2=random.randint(1,10)
 
 
-print("The L.C.M. is", compute_lcm(num1, num2))
+#equaling the correct answer with the correct lcm value
+correctAnswerLCM=compute_lcm(LCM_num1, LCM_num2)
+
+#equaling the correct answer with the correct GCD value
+correctAnswerGCD=compute_GCD(GCFnum1, GCFnum2)
+
+#user input for the lcm
+print('First Question:')
+answerLCM= int(input(f'Find the LCM of {LCM_num1} and {LCM_num2} ->'))
+
+if answerLCM==correctAnswerLCM:
+    print ('Correct ')
+else:
+    print("Incorrect the L.C.M. is", compute_lcm(LCM_num1, LCM_num2))
+
+
+#user input for the GCD
+print('Second Question:')
+answerGCD= int(input(f'Find the GCD of {GCFnum1} and {GCFnum2} ->'))
+
+if answerGCD==correctAnswerGCD:
+    print ('Correct ')
+else:
+    print("Incorrect the G.C.D is", compute_GCD(GCFnum1, GCFnum2))
+
+QUESTIONS=[
+    (f"Find the LCM of {LCM_num1} and {LCM_num2} ->",correctAnswerLCM)
+    (f'Find the GCD of {GCFnum1} and {GCFnum2} ->', correctAnswerGCD)
+]
+for answerLCM,correctAnswerLCM in QUESTIONS:
+    answer=input(f"{answerLCM}?")
+    if answer ==correctAnswerLCM:
+        print ('Correct ')
+    else:
+        print("Incorrect the L.C.M. is", compute_lcm(LCM_num1, LCM_num2))
