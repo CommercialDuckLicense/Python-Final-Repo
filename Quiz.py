3ae75ce90ceeae5f5f9fad213a6d131e5d697011
CommercialDuckLicense <voelkeltmj@gmail.com>
2025-11-27 01:09:29 -0700

    correct loops
    set up the loops. now user can choose when to move on to the next set of questions. or stay and study more on the previous kind of question.

--- Quiz.py
+++ Quiz.py
@@ -2,8 +2,6 @@ import random
 
 player=input(("Type your Name "))
 print(f'Hello {player} this is a quiz game to help with basic math questions.')
-print('Ready for the first question? ')
-
 
 # Function to find LCM
 def compute_lcm(x, y):
@@ -32,41 +30,49 @@ def compute_GCD(x, y):
             GCD = i 
     return GCD
 
-GCFnum1 =random.randint(1,10)
-GCFnum2 =random.randint(1,10)
-
-#generate random number for LCM
-LCM_num1=random.randint(1,10) 
-LCM_num2=random.randint(1,10)
-
-
-#equaling the correct answer with the correct lcm value
-correctAnswerLCM=compute_lcm(LCM_num1, LCM_num2)
-
-#equaling the correct answer with the correct GCD value
-correctAnswerGCD=compute_GCD(GCFnum1, GCFnum2)
-
-
-
-while True: # loop for when the question is wrong it loops until the answer is correct
-    #user input for the lcm
-    print('First Question:')
-    answerLCM= int(input(f'Find the LCM of {LCM_num1} and {LCM_num2} ->'))
-
-    if answerLCM==correctAnswerLCM:
-        print ('Correct ')
-        break
-    else:
-        print("Incorrect the L.C.M. is", compute_lcm(LCM_num1, LCM_num2))
-
-        
 while True:
-    #user input for the GCD
-    print('Second Question:')
-    answerGCD= int(input(f'Find the GCD of {GCFnum1} and {GCFnum2} ->'))
+    #generate random number for LCM
+    LCM_num1=random.randint(1,10) 
+    LCM_num2=random.randint(1,10)
 
-    if answerGCD==correctAnswerGCD:
-        print ('Correct ')
+    #equaling the correct answer with the correct lcm value
+    correctAnswerLCM=compute_lcm(LCM_num1, LCM_num2)
+
+    while True: # loop for when the question is wrong it loops until the answer is correct
+    #user input for the lcm
+        answerLCM= int(input(f'Find the LCM of {LCM_num1} and {LCM_num2} ->'))
+
+        if answerLCM==correctAnswerLCM:
+            print ('Correct ')
+            break
+        else:
+            print("Incorrect the L.C.M. is", compute_lcm(LCM_num1, LCM_num2))
+
+    #Ask another Answer
+    retry = input("Would you like another similar question? (y/n): ")
+    if retry != 'y':
+        print("Good work! On to the next set of questions")
         break
-    else:
-        print("Incorrect the G.C.D is", compute_GCD(GCFnum1, GCFnum2))
+
+while True:
+    GCFnum1 =random.randint(1,10)
+    GCFnum2 =random.randint(1,10)
+
+    #equaling the correct answer with the correct GCD value
+    correctAnswerGCD=compute_GCD(GCFnum1, GCFnum2)
+
+    while True:# loop for when the question is wrong it loops until the answer is correct
+        #user input for the GCD
+        answerGCD= int(input(f'Find the GCD of {GCFnum1} and {GCFnum2} ->'))
+
+        if answerGCD==correctAnswerGCD:
+            print ('Correct ')
+            break
+        else:
+            print("Incorrect the G.C.D is", compute_GCD(GCFnum1, GCFnum2))
+        #Ask another Answer
+    retry = input("Would you like another question? (y/n): ")
+    if retry != 'y':
+        print("Good work! Quiz ended.")
+        break
+
